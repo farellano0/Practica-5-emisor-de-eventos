01c3d922fa34aededd2a048dd563e54467dee0c2
Fernando Arellano <farellano0@ucol.mx>
2022-03-12 15:07:37 -0600

    initial commit_obj.nombre

---
+++ events/app.js
@@ -0,0 +1,8 @@
+let obj = {
+    nombre: 'Carlos',
+    edad: 44,
+    apellido: 'Ulibarri'
+}
+
+console.log(obj.nombre); //Muestra en la consola el nombre del objeto, en este caso Carlos.
+console.log(obj['nombre']); //Muestra en la consola el nombre del objeto.

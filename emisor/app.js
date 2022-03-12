08771fe38c05f9945f0d5e52df6d278d27838976
Fernando Arellano <farellano0@ucol.mx>
2022-03-12 16:53:40 -0600

    implemented emtr.on(jump) and final commit

--- emisor/app.js
+++ emisor/app.js
@@ -13,3 +13,10 @@ emtr.on('greet', () => {
 console.log('Hello!');
 emtr.emit('greet');
 /* En la consola se muestra el mensaje de la línea 13 y se ejecutaron las funciones de la línea 5 y 9, 'Somewhere, someone said hello.' y 'A greeting occurred!'. El método emit permite llamar a las funciones que se encuentra en la variabñe 'emtr'.*/
+
+emtr.on('jump', () => {
+    console.log('someone jumped!');
+});
+
+console.log(emtr); //Se manda a llamar la variable 'emtr' y en la consola se ve desglozado lo que contiene la variable: 2 eventos, greet y jump; el primero posee dos funciones anónimas, y el segundo solo tiene una función anónima.
+emtr.emit('jump'); // Se manda a llamar el evento 'jump' que se encuentra en la variable 'emtr' por el método 'emit' y se muestra en la conola.

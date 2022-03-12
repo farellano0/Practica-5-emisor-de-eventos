4d39ceb61f767a2cdb8d8ee819d403cb10a07b8d
Fernando Arellano <farellano0@ucol.mx>
2022-03-12 15:14:07 -0600

    myKey

--- events/app.js
+++ events/app.js
@@ -6,3 +6,7 @@ let obj = {
 
 console.log(obj.nombre); //Muestra en la consola el nombre del objeto, en este caso Carlos.
 console.log(obj['nombre']); //Muestra en la consola el nombre del objeto.
+
+let myKey = 'nombre'; //Se define una variable con el atribuno nombre del objeto creado.
+console.log(obj[myKey]); //Se muestra el nombre dek objeto que se creo con anterioridad.
+

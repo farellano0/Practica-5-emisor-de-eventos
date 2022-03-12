8bc8c127b2efae6220f19b949602cccbc1ddffa6
Fernando Arellano <farellano0@ucol.mx>
2022-03-12 15:37:07 -0600

    keypress & mouseOver

--- events/app.js
+++ events/app.js
@@ -1,12 +1,22 @@
 let obj = {
     nombre: 'Carlos',
     edad: 44,
-    apellido: 'Ulibarri'
+    apellido: 'Ulibarri',
+    keypress: function () {
+        console.log('Se ha prresionado una tecla');
+    },
+    mouseOver: function () {
+        console.log('El puntero del mouse esta arriba');
+    }
 }
 
 console.log(obj.nombre); //Muestra en la consola el nombre del objeto, en este caso Carlos.
 console.log(obj['nombre']); //Muestra en la consola el nombre del objeto.
 
 let myKey = 'nombre'; //Se define una variable con el atribuno nombre del objeto creado.
-console.log(obj[myKey]); //Se muestra el nombre dek objeto que se creo con anterioridad.
+console.log(obj[myKey]); //Se muestra el nombre dek objeto que se creo con anterioridad. 
 
+myKey = 'keypress';
+console.log(obj[myKey]());
+myKey = 'mouseOver';
+console.log(obj[myKey]());

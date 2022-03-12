3a8862995b434aac1d9b3fe1ca38a7eef3ec0576
Fernando Arellano <farellano0@ucol.mx>
2022-03-12 15:43:47 -0600

    myArray

--- events/app.js
+++ events/app.js
@@ -17,6 +17,19 @@ let myKey = 'nombre'; //Se define una variable con el atribuno nombre del objeto
 console.log(obj[myKey]); //Se muestra el nombre dek objeto que se creo con anterioridad. 
 
 myKey = 'keypress';
-console.log(obj[myKey]());
+console.log(obj[myKey]()); //Se muestra la función del atributo 'keypress' ya que la variable myKey cambió su atributo 'nombre' por el atributo 'keypress'.
 myKey = 'mouseOver';
 console.log(obj[myKey]());
+
+let myArray = [];
+
+myArray.push(4);
+myArray.push('una cadena');
+myArray.push(obj);
+myArray.push(function () {
+    console.log('Hola desde el array');
+});
+
+console.log(myArray); //Se muestra el vector que se creo con los datos que se fueron ingresando de la linea 26 a la linea 29. Se observa el 4, una cadena, un objeto y una función, aunque esta última se presente como función anónima.
+
+
